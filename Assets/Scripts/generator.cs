8dd5f6cdbca7dc8ecbd09e6af98908ce6dc1e1aa
Jonathan <jonathan@bangert.dk>
2022-05-02 18:32:01 +0200

    lots of changes

--- Assets/Scripts/generator.cs
+++ Assets/Scripts/generator.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class generator : MonoBehaviour
 {
-    public GameObject[] prefabs;
-    public int level_parts;
+    public GameObject[] predeterment;
+    public GameObject[] basic;
+    public int level_parts = 0;
     public int levels_at_a_time;
     private int last;
     private int last2;
-    public float z_cord;
-    public float y_cord;
+    public int z_cord;
+    public int y_cord;
     public float level_space;
     public GameObject clone_ref;
+    public int speed;
+    public int nexo = 0;
     // Start is called before the first frame update
     void Start()
     {
-        new_level(0);
+        new_level(0, 1, "pre");
     }
 
     // Update is called once per frame
@@ -24,28 +27,80 @@ public class generator : MonoBehaviour
     {
         if (level_parts < levels_at_a_time)
         {
+            int fspeed;
+            int ftspeed = 1;
+            if (speed < 5)
+            {
+                fspeed = speed;
+            }
+            else
+            {
+                fspeed = 4;
+            }
+            if (speed < 4)
+            {
+                ftspeed = speed;
+            }
+            else
+            {
+                ftspeed = 3;
+            }
+            print ("ft");
+            print (ftspeed);
+            print ("f");
+            print (fspeed);
             int nn;
             do
             {
                 nn = Random_range();
             } while ((nn == last) || (nn == last2));
-            z_cord = z_cord + (20 * level_space);
-            y_cord = y_cord -5;
-            new_level(nn);
-            level_parts ++;
+            if (nexo < ftspeed)
+            {
+                new_level(nn, fspeed, "bas");
+                nexo ++;
+            }
+            else
+            {
+                new_level(1, 1, "pre");
+                speed ++;
+                nexo = 0;
+            }
             last2 = last;
             last = nn;
         }
     }
-    void new_level(int x)
+    void new_level(int index, int number_of_types_to_spawn, string type_of_array)
     {
-        var clone = Instantiate(prefabs[x], new Vector3(0, y_cord, z_cord), Quaternion.identity);
-        clone.GetComponent<delete_check>().cam = clone_ref;
-        clone.GetComponent<delete_check>().spawn = this;
+        GameObject clone = null;
+        if (number_of_types_to_spawn == 0)
+        {
+            number_of_types_to_spawn = 1;
+        }
+        for (int i = 1; i <= number_of_types_to_spawn; i++)
+        {
+            if (type_of_array == "bas")
+            {
+                clone = Instantiate(basic[index], new Vector3(0, y_cord, z_cord), Quaternion.identity);
+                Debug.Log("bas");
+            }
+            else
+            {
+                clone = Instantiate(predeterment[index], new Vector3(0, y_cord, z_cord), Quaternion.identity);
+                Debug.Log("pre");
+            }
+            if (clone != null)
+            {
+                clone.GetComponent<delete_check>().cam = clone_ref;
+                clone.GetComponent<delete_check>().spawn = this;
+            }
+            z_cord = (Mathf.RoundToInt(z_cord + 20 * level_space));
+            y_cord = y_cord -8;
+            level_parts ++;
+        }
     }
     int Random_range()
     {
-        int nextpr = Random.Range(1, prefabs.Length);
+        int nextpr = Random.Range(1, basic.Length);
         return nextpr;
     }
 }
